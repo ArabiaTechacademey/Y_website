f04bdf2af51462eaf7e64ee03ea8cafa24d6c9ae
ArabiaTechacademey <arabiatechteamacademy@gmail.com>
2026-08-03 19:36:57 +0300

    Update books-data.js

--- static/js/books-data.js
+++ static/js/books-data.js
@@ -1,1 +1,11 @@
-
+const BOOKS = [
+  {
+    id: "سفر عبر الزمن",
+    title: " الملك و العهد",
+    subtitle: "رواية",
+    description: "داخل تابوتٍ فرعوني بدأت الحكاية، وبين طيات الزمن أختار مراد أن يكتب نهايته... فهل يُهزم الموت بأسرار الماضي؟",
+    cover: "static/images/book-1.png",
+    pdf: "static/pdfs/book-1.pdf",
+    accent: "gold"
+  }
+];
