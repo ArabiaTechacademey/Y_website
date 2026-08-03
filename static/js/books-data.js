6a85db23a351b40ed514f6bc46e5be7a14412caf
ArabiaTechacademey <arabiatechteamacademy@gmail.com>
2026-08-03 20:23:07 +0300

    Update books-data.js

--- static/js/books-data.js
+++ static/js/books-data.js
@@ -5,7 +5,7 @@ const BOOKS = [
     subtitle: "رواية",
     description: "داخل تابوتٍ فرعوني بدأت الحكاية، وبين طيات الزمن أختار مراد أن يكتب نهايته... فهل يُهزم الموت بأسرار الماضي؟",
     cover: "static/images/book-1.png",
-    driveId: "19hdEPSFQ4iUhtWoXHqg2E1kPCpUmaEgP8", // الـ ID بس، مش الرابط كامل
+    driveId: "14ysGmnIl92S5C6r1Dyt8s-G-74ajrJQS", // الـ ID بس، مش الرابط كامل
     accent: "gold"
   }
 ];
